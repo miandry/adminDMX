63be55a1c9a63daa1aa147eeda2a008dd297325e
tokyNomenjanahary <razananirianatoky@gmail.com>
2025-11-28 12:27:57 +0300

    filter history

--- vue-app/src/components/cardTransaction/HistoryPannel.vue
+++ vue-app/src/components/cardTransaction/HistoryPannel.vue
@@ -1,15 +1,50 @@
 <template>
     <!-- History Slide Panel -->
     <div>
-        <div class="flex items-center justify-between p-4 border-b border-gray-200">
-            <h3 class="text-lg font-semibold text-gray-900">
-                Historique des Calculs
-            </h3>
-            <button class="text-gray-400 hover:text-gray-600" @click="$emit('close')">
-                <i class="ri-close-line text-xl"></i>
-            </button>
+        <div class="border-b border-gray-200 p-4">
+            <div class="flex items-center justify-between">
+                <h3 class="text-lg font-semibold text-gray-900">
+                    Historique des Calculs
+                </h3>
+                <button class="text-gray-400 hover:text-gray-600" @click="$emit('close')">
+                    <i class="ri-close-line text-xl"></i>
+                </button>
+            </div>
+            <div class="flex gap-2 mt-3">
+                <div>
+                    <button @click="dateSort"
+                        class="bg-primary text-white py-1 px-2 !rounded-button text-xs font-medium">
+                        <i :class="[sortValue == 'desc' ? 'ri-sort-asc' : 'ri-sort-desc']"></i></button>
+                </div>
+                <div class="relative">
+                    <input type="text" placeholder="Recherche par client" @keyup="searchClient"
+                        v-model="searchKeywordClient"
+                        class="w-full px-3 py-1 border border-gray-300 rounded-lg text-xs" />
+                    <i @click="selectClient('','')"
+                        class="ri-close-line text-xl absolute right-1 top-1/2 transform -translate-y-1/2 text-gray-400 cursor-pointer"></i>
+                    <div v-if="showList"
+                        class="max-h-48 overflow-y-auto border border-gray-300 !rounded-button bg-white absolute right-0 left-0 z-30">
+                        <div v-if="clientStore.clients.rows.length" class="divide-y divide-gray-100">
+                            <div v-for="(client, index) in clientStore.clients.rows" :key="index" :class="[
+                                'flex items-center space-x-3 px-2 py-1 hover:bg-gray-50 cursor-pointer customer-item border-t-0'
+                            ]" @click="selectClient(client.nid, client.title)">
+                                <div class="flex-1">
+                                    <p class="text-xs font-medium text-gray-900">{{ client.title }}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div v-else>
+                            <h3 class="text-center text-gray-400 py-2">Aucun client trouvé</h3>
+                        </div>
+                    </div>
+                </div>
+                <div>
+                    <input type="date" placeholder="Selectionner une date" v-model="rangeValue" @change="dateRange"
+                        class="w-full px-3 py-1 border border-gray-300 rounded-lg text-xs" />
+                </div>
+            </div>
         </div>
-        <div class="overflow-y-auto h-full pb-4">
+        <div class="overflow-y-auto h-full pb-4" ref="scrollContainer">
             <div class="p-4 space-y-3 mb-20" v-if="transactionStore.transactions.rows.length">
                 <!-- Example calculations -->
                 <div v-for="transaction in transactionStore.transactions.rows" :key="transaction.nid"
@@ -89,11 +124,19 @@
 <script setup>
 import { ref, watch } from 'vue';
 import { useTransactionStore } from '../../stores/transaction/transaction.js';
+import { useClientStore } from '../../stores/index.js';
 
 const transactionStore = useTransactionStore();
 const emit = defineEmits(['openDetails', 'openConfirmDelete', 'close']);
 // Déterminer si on peut charger plus
 const canLoadMore = ref(false);
+// filter
+const sortValue = ref('desc')
+const scrollContainer = ref(null);
+const rangeValue = ref('')
+const showList = ref(false);
+const clientStore = useClientStore();
+const searchKeywordClient = ref('');
 // Paramètres dynamiques de la requête
 const queryOptions = ref({
     fields: [
@@ -113,6 +156,19 @@ const queryOptions = ref({
     filters: {},
     pager: 0,
     offset: 10
+})
+
+
+const queryOptionsClient = ref({
+    fields: [
+        'nid',
+        'title',
+        'field_phone',
+    ],
+    sort: { val: 'title', op: 'desc' },
+    filters: {},
+    pager: 0,
+    offset: 100
 })
 
 const fetchTransactions = async (append = false) => {
@@ -146,6 +202,66 @@ const goToDetails = (id) => {
 
 const sendEditData = (transaction) => {
     emit('close', transaction)
+}
+
+const dateSort = () => {
+    if (sortValue.value == 'desc') {
+        sortValue.value = 'asc'
+    } else {
+        sortValue.value = 'desc'
+    }
+
+    // Mettre à jour le sort de la requête
+    queryOptions.value.sort = { val: 'nid', op: sortValue.value }
+    fetchTransactions().then(() => {
+        scrollToTop();
+    })
+}
+
+const dateRange = () => {
+    updateFilter('created', toTimestamp(rangeValue.value), '>=', 'transaction')
+    fetchTransactions().then(() => {
+        scrollToTop();
+    })
+}
+
+const selectClient = async (nid, name) => {
+    updateFilter('field_client', nid, '=', 'transaction')
+    fetchTransactions().then(() => {
+        scrollToTop();
+    })
+    searchKeywordClient.value = name;
+    showList.value = false;
+}
+
+function scrollToTop() {
+    if (scrollContainer.value) {
+        scrollContainer.value.scrollTop = 0;
+    }
+}
+
+const updateFilter = (key, value, op = '=', type) => {
+    if (type == 'client') {
+        if (!value) delete queryOptionsClient.value.filters[key]
+        else queryOptionsClient.value.filters[key] = { val: value, op }
+    }
+    if (type == 'transaction') {
+        if (!value) delete queryOptions.value.filters[key]
+        else queryOptions.value.filters[key] = { val: value, op }
+    }
+}
+
+function toTimestamp(dateString) {
+    return Math.floor(new Date(dateString).getTime() / 1000);
+}
+
+const searchClient = async () => {
+    updateFilter('title', searchKeywordClient.value, 'CONTAINS', 'client')
+    await clientStore.fetchClients(queryOptionsClient.value);
+    showList.value = true;
+    if (searchKeywordClient.value == '') {
+        showList.value = false;
+    }
 }
 
 defineExpose({
