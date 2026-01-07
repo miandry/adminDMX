ab8c0767259c3d80abc195ec3c56755de76b9073
tokyNomenjanahary <razananirianatoky@gmail.com>
2026-01-07 19:57:37 +0300

    test local ok

--- vue-app/vite.config.js
+++ vue-app/vite.config.js
@@ -4,29 +4,47 @@ import { resolve } from "path";
 import { VitePWA } from "vite-plugin-pwa";
 
 export default defineConfig({
-  base: "/",
+  base: "/drupal/transaction.local/",
   plugins: [
     vue(),
     VitePWA({
-      registerType: "autoUpdate",
+      registerType: "manual",
       strategies: "generateSW",
-      injectRegister: false,
+      filename: "sw.js",
+      navigateFallback: null,
       manifest: {
         name: "Drupal Vue PWA",
         short_name: "VuePWA",
-        start_url: "/",
-        scope: "/",
+        start_url: "/drupal/transaction.local/",
+        scope: "/drupal/transaction.local/",
         display: "standalone",
         background_color: "#ffffff",
         theme_color: "#0f172a",
       },
 
       workbox: {
+        globPatterns: ["themes/adminDMX/dist/**/*.{js,css,webmanifest}"],
         cleanupOutdatedCaches: true,
         clientsClaim: true,
         skipWaiting: true,
-
+        navigateFallback: null,
         runtimeCaching: [
+          // 🔹 CDN CSS / JS
+          {
+            urlPattern:
+              /^https:\/\/(cdn\.tailwindcss\.com|cdnjs\.cloudflare\.com|fonts\.googleapis\.com|fonts\.gstatic\.com)\//,
+            handler: "StaleWhileRevalidate",
+            options: {
+              cacheName: "cdn-assets",
+              expiration: {
+                maxEntries: 50,
+                maxAgeSeconds: 60 * 60 * 24 * 30, // 30 jours
+              },
+              cacheableResponse: {
+                statuses: [0, 200], // 0 = opaque response (CDN)
+              },
+            },
+          },
           {
             urlPattern: ({ request }) => request.destination === "document",
             handler: "NetworkFirst",
